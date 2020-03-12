d954bd921e90225088ae826a8a951c4526d1a992
Christopher Le <locvicvn1234@gmail.com>
2020-03-11 23:12:11 -0500

    update

--- Blue/FLOYD-WARSHALL_ALGORITHM/Meeting_Prof_Miguel.py
+++ Blue/FLOYD-WARSHALL_ALGORITHM/Meeting_Prof_Miguel.py
@@ -1,22 +1,25 @@
 import queue
-MAX = 100
+MAX = 30
 INF = int(1e9)
 
 
 def FloyWarshall(graph, dist, path):
-    for i in range(n):
-        for j in range(n):
+    for i in range(MAX):
+        for j in range(MAX):
             # print(graph[i][j])
-            dist[i][j] = graph[i][j]
+            if i == j:
+                dist[i][j] = 0
+            else:
+                dist[i][j] = graph[i][j]
 
             if graph[i][j] != INF and i != j:
                 path[i][j] = i
             else:
                 path[i][j] = -1
 
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
+    for k in range(MAX):
+        for i in range(MAX):
+            for j in range(MAX):
                 if dist[i][j] > dist[i][k] + dist[k][j]:
                     dist[i][j] = dist[i][k] + dist[k][j]
                     path[i][j] = path[k][j]
@@ -45,21 +48,27 @@ while n != 0:
         end = ord(arr[3]) - ord('A')
 
         energy = int(arr[4])
+        # print(age, type)
 
         # Young
         if age == 24:
+            # B
             if type == 1:
                 graph_young[source][end] = min(graph_young[source][end], energy)
                 graph_young[end][source] = min(graph_young[end][source], energy)
 
+            # U
             else:
                 graph_young[source][end] = min(graph_young[source][end], energy)
 
+        # Old
         if age == 12:
+            # B
             if type == 1:
                 graph_old[source][end] = min(graph_old[source][end], energy)
                 graph_old[end][source] = min(graph_old[end][source], energy)
 
+            # U
             else:
                 graph_old[source][end] = min(graph_old[source][end], energy)
 
@@ -76,11 +85,13 @@ while n != 0:
         res_y = dist_young[s_young][f]
         res_o = dist_old[s_old][f]
 
+        # print(res_y, res_o)
+        # print()
         sum_arr.append((res_y + res_o, f))
 
     result, index_value = min(sum_arr)
 
-    if result > INF:
+    if result >= INF:
         print("You will never meet.")
     else:
         print(result, chr(index_value + ord('A')))
